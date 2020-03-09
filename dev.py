e7f2e59cbb5a55adb5ed5e2092d2dc1cb8c28a8b
izacdev <60254452+izacdev@users.noreply.github.com>
2020-03-09 17:14:02 +0300

    my solution  to detect

---
+++ dev.py
@@ -0,0 +1,15 @@
+print('hi devs')
+
+name = input("What is your name")
+School = input("What is School")
+subject = input("what is your combination")
+score = input("Ho many point did you score?")
+print(name + ' studied at ' + School + ' and did ' + subject + ' he scored ' + score)
+
+pass_exam_Physics = True
+pass_exams_chemical = True
+
+if pass_exam_Physics and pass_exams_chemical:
+   
+    print(name + ' you qualify for scholarship ')
+ 
